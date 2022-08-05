c0b96d54074cf5f51927140404d7b892d194cd1c
Dmitryi <dima2581992@mail.ru>
2022-08-05 22:51:38 +0300

    write programm

---
+++ Task/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array1 = new string[8] { "name", "fio", "prof", "world", "QA", "234", "QAT", "programm" };   // задаем 2 массива одинаковой длины
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2) // функция проверки первого массива заданному условию, в данном случае - проверка длины строк
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array) // функция вывода массива на печать
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+SecondArray(array1, array2);
+PrintArray(array2);
+Console.WriteLine();
